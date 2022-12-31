a5a24452d50b407abbfdd6169f19d95f921ee229
vinee <vineeisnotbad@gmail.com>
2022-12-31 15:00:40 +0530

    env config updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBPdmar_GwUf5BvLAvOU4zl3zL3EKrmHZ8",
+    authDomain: "rareapps-40558.firebaseapp.com",
+    projectId: "rareapps-40558",
+    storageBucket: "rareapps-40558.appspot.com",
+    messagingSenderId: "508643499075",
+    appId: "1:508643499075:web:406bd1bbe9090d86cf48f6",
+    measurementId: "G-XX53N1G46F"
+  }
 };
